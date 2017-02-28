f96f2f83a00025c327e15af2ae14ac46b6e376e8
jacksunjian <877921009@qq.com>
2017-02-28 22:47:32 +0800

    add search activity

---
+++ app/bluecar/src/main/java/com/blue/car/activity/SearchActivity.java
@@ -0,0 +1,29 @@
+package com.blue.car.activity;
+
+import com.blue.car.R;
+
+/**
+ * Created by Administrator on 2017/2/28.
+ */
+
+public class SearchActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_search;
+    }
+
+    @Override
+    protected void initConfig() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
