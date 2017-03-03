47e6696b9fd33c74cb9ac00d42e63c6c2c355125
sunj <123456>
2017-03-03 13:22:08 +0800

    add some xmls

---
+++ app/bluecar/src/main/java/com/blue/car/activity/SearchResultActivity.java
@@ -0,0 +1,56 @@
+package com.blue.car.activity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.blue.car.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Created by admin on 2017/3/2.
+ */
+
+public class SearchResultActivity extends BaseActivity {
+    @Bind(R.id.lh_tv_title)
+    TextView lhTvTitle;
+    @Bind(R.id.paired_devices)
+    ListView pairedDevices;
+    @Bind(R.id.button_scan)
+    Button buttonScan;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_search_result;
+    }
+
+    @Override
+    protected void initConfig() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button_scan)
+    public void onClick() {
+    }
+}
