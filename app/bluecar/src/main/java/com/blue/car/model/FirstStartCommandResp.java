a54a9ebbda7a16c4c91ac9658b1a75edaff28a66
eling13 <490106525@qq.com>
2017-05-25 15:16:44 +0800

    fix: modify fisrtStartCommandResp

--- app/bluecar/src/main/java/com/blue/car/model/FirstStartCommandResp.java
+++ app/bluecar/src/main/java/com/blue/car/model/FirstStartCommandResp.java
@@ -1,5 +1,7 @@
 package com.blue.car.model;
 
+import com.blue.car.utils.StringUtils;
+
 public class FirstStartCommandResp {
     public String carId; //要求前6位必须是11960/ 或者 201702
     public String blePassword;
@@ -9,4 +11,11 @@ public class FirstStartCommandResp {
     public int warningCode;
     public int sysStatus;
     public byte[] remain;
+
+    public boolean isCardIdValid() {
+        if (StringUtils.isNullOrEmpty(carId)) {
+            return false;
+        }
+        return carId.startsWith("11960") || carId.startsWith("201702");
+    }
 }
