b535e2c6add5dfca79a1e17e00aa4a7c8228e79c
sunjian2 <877921009@qq.com>
2017-05-27 17:39:15 +0800

    change the message tip

--- app/bluecar/src/main/java/com/blue/car/activity/BlackBoxActivity.java
+++ app/bluecar/src/main/java/com/blue/car/activity/BlackBoxActivity.java
@@ -94,7 +94,7 @@ public class BlackBoxActivity extends BaseActivity {
 
     private void showCarLockDialog() {
         new MaterialDialog.Builder(this)
-                .content("黑匣子读取第一步要先锁车")
+                .content("黑匣子读取需要锁车，是否继续？")
                 .positiveText("确认")
                 .negativeText("取消")
                 .negativeColor(Color.GRAY)
